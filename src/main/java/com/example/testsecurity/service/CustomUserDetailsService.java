6fdb007b04ea04e157f9fb66ef33b07c8b0dbb8e
Logy-CTO <hong2hwa58@gmail.com>
2024-02-10 01:03:37 +0900

    스프링 시큐리티 8 : DB기반 로그인 검증 로직

--- src/main/java/com/example/testsecurity/service/CustomUserDetailsService.java
+++ src/main/java/com/example/testsecurity/service/CustomUserDetailsService.java
@@ -9,6 +9,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+
+// "/login" 경로로 POST 요청이 오면 스프링 시큐리티 내부적으로 UsernamPasswordAuthenticationFilter가 동작을 하게되고
+// 이때 AuthenticationProvider에 의해 CustomUserDetailsService의 loadUserByUsername을 호출하여 DB에 있는 유저를 조회하게 됩니다.
+
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
